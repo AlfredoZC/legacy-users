47d8f91f2cf77be073f6c1992121a7a65ccd4b12
AlfredoZC <josealfredo.zambranacruz.az@gmail.com>
2026-05-20 20:09:47 -0400

    fix: usar data source para AMI dinamica de Amazon Linux 2023

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,5 +1,19 @@
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
 resource "aws_instance" "app_server" {
-  ami                    = var.ami_id
+  ami =             data.aws_ami.amazon_linux_2023.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [var.security_group_id]
   iam_instance_profile   = var.iam_instance_profile
